0c3e7dcc4a39723115fc1adcdb81ba999f40469c
pastore99 <c.pastore35@studenti.unisa.it>
2024-02-21 17:39:36 +0100

    add element to giardino

--- src/GiardinoHome.js
+++ src/GiardinoHome.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Carousel from '../src/components/Carousel/Carousel';
 import Navbar from '../src/components/navbar/navbar';
+import Header from '../src/components/headerGiardino/headerGiardino';
 import Card from '../src/components/card/card';
 import Footer from '../src/components/footer/footer';
 import immagine1 from "./image/immagine1.JPG"
@@ -14,6 +15,8 @@ import Navigazione from "./Navigazione";
 import Home from "./App";
 import Giardino from "./GiardinoHome";
 import vittime from "./vittime.json";
+import { Link } from 'react-router-dom';
+
 function GiardinoHome() {
     useEffect(() => {
         // Stampare i dati in console
@@ -22,25 +25,23 @@ function GiardinoHome() {
 
     return (
         <div>
-            <Navbar/>
-
+            <Header/>
+            <div className=" mt-4">
+                <h1  className=" text-center">Il giardino della legalità "Falcone e Borsellino" </h1>
+                <p  className=" mx-5">
+                    Nel Giardino di Ulivi dedicato a Borsellino e Falcone, ogni albero è un monumento vivente, testimone delle vite spezzate dalla mafia. Ogni tronco, ogni ramo, ogni foglia racconta la storia di una vittima, radicando nel terreno la memoria e il coraggio di coloro che hanno combattuto per la legalità. In questo giardino sacro, gli ulivi sono simboli di speranza e rinascita, sprigionando nell'aria un profumo di resilienza e dignità, affinché il ricordo delle vittime continui a illuminare il cammino verso un futuro senza mafia.</p>
+            </div>
             <Container>
-                {vittime.map(vittima =>(
-                    <Card key
-                ))}
-                {/*<Row className="justify-content-center my-4 mx-0">*/}
-                {/*    {Array.isArray(vittimeData) && vittimeData.map((vittima, index) => (*/}
-                {/*        <Col key={index} className="text-center d-flex justify-content-center">*/}
-                {/*            <Card title={vittima.nome} // Assumi che il nome della vittima sia il titolo*/}
-                {/*                  imageUrl={immagine1} // Immagine di esempio, puoi passare l'URL dell'immagine dal JSON se presente*/}
-                {/*                  text={vittima.descrizione} // Testo della descrizione dalla vittima*/}
-                {/*                  btnText={"Scopriamolo"}*/}
-                {/*                // Se necessario, puoi aggiungere un link alla pagina della vittima*/}
-                {/*                // <Link to={`/vittime/${vittima.id}`}>Scopriamolo</Link>*/}
-                {/*            />*/}
-                {/*        </Col>*/}
-                {/*    ))}*/}
-                {/*</Row>*/}
+                <Row className="justify-content-center my-4 mx-0 m-4">
+                    {Array.isArray(vittime.vittime_mafia) && vittime.vittime_mafia.map((vittima, index) => (
+                        <Col key={index} xs={12} sm={6} md={4} lg={3} className="mb-4">
+                            <Card title={vittima.nome} // Assumi che il nome della vittima sia il titolo
+                                  imageUrl={immagine1} // Immagine di esempio, puoi passare l'URL dell'immagine dal JSON se presente
+                                  text={vittima.descrizione} // Testo della descrizione dalla vittima
+                                  btnText={"Scopriamolo"}/>
+                          </Col>
+                    ))}
+                </Row>
             </Container>
             <Footer/>
         </div>
