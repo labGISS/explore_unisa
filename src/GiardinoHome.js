cd0d9100fb860dcd728fc4e3dfd21091d78f6624
pastore99 <c.pastore35@studenti.unisa.it>
2024-02-21 15:53:21 +0100

    home finita

--- src/GiardinoHome.js
+++ src/GiardinoHome.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Carousel from '../src/components/Carousel/Carousel';
 import Navbar from '../src/components/navbar/navbar';
 import Card from '../src/components/card/card';
@@ -12,42 +13,38 @@ import SimpleMap from "./SimpleMap";
 import Navigazione from "./Navigazione";
 import Home from "./App";
 import Giardino from "./GiardinoHome";
+import vittime from "./vittime.json";
+function GiardinoHome() {
+    useEffect(() => {
+        // Stampare i dati in console
+        console.log(vittime);
+    }, []);
 
-
-
-function GiardinoHome(){
     return (
         <div>
             <Navbar/>
-            <Carousel/>
-            <Row className="justify-content-center my-4 mx-0">
-                <Col className="text-center d-flex justify-content-center">
-                    <Card title={"Giardino della legalità"}
-                          imageUrl={immagine1}
-                          text={"questa è una prova"}
-                          btnText={"Scopriamolo"}
-                         />
-                </Col>
-                <Col className="text-center d-flex justify-content-center">
-                    <Card title={"Giardino della legalità"}
-                          imageUrl={immagine1}
-                          text={"questa è una prova"}
-                          btnText={"Scopriamolo"}
-                        />
-                </Col>
-                <Col className="text-center d-flex justify-content-center">
-                    <Card title={"Giardino della legalità"}
-                          imageUrl={immagine1}
-                          text={"questa è una prova"}
-                          btnText={"Scopriamolo"}
-                         />
-                </Col>
-            </Row>
+
+            <Container>
+                {vittime.map(vittima =>(
+                    <Card key
+                ))}
+                {/*<Row className="justify-content-center my-4 mx-0">*/}
+                {/*    {Array.isArray(vittimeData) && vittimeData.map((vittima, index) => (*/}
+                {/*        <Col key={index} className="text-center d-flex justify-content-center">*/}
+                {/*            <Card title={vittima.nome} // Assumi che il nome della vittima sia il titolo*/}
+                {/*                  imageUrl={immagine1} // Immagine di esempio, puoi passare l'URL dell'immagine dal JSON se presente*/}
+                {/*                  text={vittima.descrizione} // Testo della descrizione dalla vittima*/}
+                {/*                  btnText={"Scopriamolo"}*/}
+                {/*                // Se necessario, puoi aggiungere un link alla pagina della vittima*/}
+                {/*                // <Link to={`/vittime/${vittima.id}`}>Scopriamolo</Link>*/}
+                {/*            />*/}
+                {/*        </Col>*/}
+                {/*    ))}*/}
+                {/*</Row>*/}
+            </Container>
             <Footer/>
         </div>
-    )
-
-
+    );
 }
 
 export default GiardinoHome;
